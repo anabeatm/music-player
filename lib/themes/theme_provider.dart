54b564769aab86b5b2644114bf1662c3a8459e7c
Ana Malaquias <ana.beat20051@gmail.com>
2026-03-30 13:09:45 -0300

    feat: implement ThemePreset class for customizable theme selection

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:minimal_music_player/themes/light_mode.dart';
 import 'package:minimal_music_player/themes/dark_mode.dart';
 
+// implementando seleção de temas para o app
+class ThemePreset {
+  final String name;
+  final ThemeData lightTheme;
+  final ThemeData darkTheme;
+
+  ThemePreset({
+    required this.name,
+    required this.lightTheme,
+    required this.darkTheme,
+  });
+}
+
+// TODO: modificar classe para escolha de temas personalizadas
 class ThemeProvider extends ChangeNotifier {
   // light mode
   ThemeData _themeData = lightMode;
